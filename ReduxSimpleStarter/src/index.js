8adddb81d3f89bc220d19d83978316ea1c1061ee
Robin Yan <robin.yan@jet.com>
2018-03-02 22:04:23 -0500

    Converted function component to class component

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
-
 import SearchBar from "./components/search_bar";
-
+import VideoList from "./components/video_list";
 const API_KEY = "AIzaSyCwa_Fl_ivA48nSJ5gXEBzJmT9h9S3_1vU";
-
-YTSearch({ key: API_KEY, term: "surfboards" }, function(data) {
-  console.log(data);
-});
 
 // Create a new component. This component should produce some HTML
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+      this.setState({ videos });
+      //this.setState({ videos : videos});
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos }/>
+      </div>
+    );
+  }
+}
 // Take this component's generated HTML and put it on the page (in the DOM )
 ReactDOM.render(<App />, document.querySelector(".container"));
