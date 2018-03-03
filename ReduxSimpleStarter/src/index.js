83a0b272a1810c419b53e91b3aa8831769f5415a
Robin Yan <robin.yan@jet.com>
2018-03-02 21:13:22 -0500

    Added Youtube search

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/search_bar";
 
 const API_KEY = "AIzaSyCwa_Fl_ivA48nSJ5gXEBzJmT9h9S3_1vU";
+
+YTSearch({ key: API_KEY, term: "surfboards" }, function(data) {
+  console.log(data);
+});
 
 // Create a new component. This component should produce some HTML
 
