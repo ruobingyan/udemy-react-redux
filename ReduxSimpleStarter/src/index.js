a5b187849eedf1e358370534e0805b1e605ba5eb
Robin Yan <robin.yan@jet.com>
2018-03-03 22:03:39 -0500

    Added null handling
    Added video_detail

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 const API_KEY = "AIzaSyCwa_Fl_ivA48nSJ5gXEBzJmT9h9S3_1vU";
 
 // Create a new component. This component should produce some HTML
@@ -23,7 +24,8 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos }/>
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
