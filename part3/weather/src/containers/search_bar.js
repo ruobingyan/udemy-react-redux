4243446ac217229ab0f85022e0be9ce04d75149d
Robin Yan <robin.yan@jet.com>
2018-03-11 22:47:14 -0400

    Added form onSubmit event handler for submit button, preventing submit

--- part3/weather/src/containers/search_bar.js
+++ part3/weather/src/containers/search_bar.js
@@ -11,12 +11,18 @@ export default class SearchBar extends Component {
 
   onInputChange(event) {
     console.log(event.target.value);
-    this.setState( {term : event.target.value});
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    // we need to go and fetch weather data
   }
 
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forecase in your favorite cities"
           className="form-control"
