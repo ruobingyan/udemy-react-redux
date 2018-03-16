5cb2f4f59b6f65a3bd323fe9a2a70a6b8e8caec7
Robin Yan <robin.yan@jet.com>
2018-03-15 22:18:14 -0400

    Replaced city name displaying with GoogleMap map

--- part3/weather/src/containers/weather_list.js
+++ part3/weather/src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Chart from "../components/chart";
+import GoogleMap from "../components/google_map";
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -8,13 +9,22 @@ class WeatherList extends Component {
     const temps = cityData.list.map(weather => weather.main.temp);
     const presure = cityData.list.map(weather => weather.main.pressure);
     const humidity = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
 
     return (
       <tr key={name}>
-        <td>{name}</td>
-        <td><Chart data={temps} color="orange" units="K" /></td>
-        <td><Chart data={presure} color="green" units="hPa" /></td>
-        <td><Chart data={humidity} color="black" units="%" /></td>
+        <td>
+          <GoogleMap lon={lon} lat={lat} />
+        </td>
+        <td>
+          <Chart data={temps} color="orange" units="K" />
+        </td>
+        <td>
+          <Chart data={presure} color="green" units="hPa" />
+        </td>
+        <td>
+          <Chart data={humidity} color="black" units="%" />
+        </td>
       </tr>
     );
   }
