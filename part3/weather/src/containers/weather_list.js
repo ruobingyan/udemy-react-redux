175dd1161ad341c268a153206c5e8b2f253841ff
Robin Yan <robin.yan@jet.com>
2018-03-15 21:34:39 -0400

    Added pressure and humidity

--- part3/weather/src/containers/weather_list.js
+++ part3/weather/src/containers/weather_list.js
@@ -6,14 +6,15 @@ class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
-    console.log(temps);
+    const presure = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Chart data={temps} color="orange" />
-        </td>
+        <td><Chart data={temps} color="orange" /></td>
+        <td><Chart data={presure} color="green" /></td>
+        <td><Chart data={humidity} color="black" /></td>
       </tr>
     );
   }
