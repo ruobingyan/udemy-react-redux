85567549452420e6c193ad263f63362c259ab8ac
Robin Yan <robin.yan@jet.com>
2018-03-17 21:50:07 -0400

    Added code getting posts from remote endpoint

--- part4/ReduxSimpleStarter/src/components/posts_index.js
+++ part4/ReduxSimpleStarter/src/components/posts_index.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 
 class PostsIndex extends Component {
-  render() {
-    return (
-      <div>
-        Posts index
-      </div>
-    );
+  componentDidMount() {     // life cycle method
+    this.props.fetchPosts();
   }
+
+  render() {
+    return <div>Posts index</div>;
+  };
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
