89a0e78364e19390e37d2059577a5e4e1bf5b827
Robin Yan <robin.yan@jet.com>
2018-03-17 22:13:37 -0400

    Updating post head

--- part4/ReduxSimpleStarter/src/reducers/index.js
+++ part4/ReduxSimpleStarter/src/reducers/index.js
@@ -1,5 +1,5 @@
-import { combineReducers } from 'redux';
-import PostsReducer from './reducer_posts';
+import { combineReducers } from "redux";
+import PostsReducer from "./reducer_posts";
 
 const rootReducer = combineReducers({
   posts: PostsReducer
