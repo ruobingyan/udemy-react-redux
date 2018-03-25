e4cc7d00a93741a8bf520588d7d5f809a0d23059
Robin Yan <robin.yan@jet.com>
2018-03-24 21:28:54 -0400

    Added Link button return to root from details page

--- part4/ReduxSimpleStarter/src/components/posts_show.js
+++ part4/ReduxSimpleStarter/src/components/posts_show.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { fetchPost } from "../actions";
 
 class PostsShow extends Component {
@@ -15,6 +16,9 @@ class PostsShow extends Component {
     }
     return (
       <div>
+        <Link to="/" className="btn btn-primary">
+          Back To Index
+        </Link>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -24,7 +28,7 @@ class PostsShow extends Component {
 }
 
 function mapStateToProps({ posts }, ownProps) {
-  return { post: posts[ownProps.match.params.id]};
+  return { post: posts[ownProps.match.params.id] };
 }
 
 export default connect(mapStateToProps, { fetchPost })(PostsShow);
